a6f3e7274c3d5c7d0932f3fb6abef606f31a7e40
John Tsiombikas <nuclear@mutantstargoat.com>
2017-04-03 10:14:34 +0300

    found the byte-transfer bug. Need to use odd I/O addresses the way I wired up the AVR in the I/O board

--- test2/src/test.s
+++ test2/src/test.s
@@ -13,7 +13,7 @@ printstr:
 	move.b (%a0)+, %d0
 	tst.b %d0
 	beq.s 0f
-	move.b %d0, 0x8000
+	move.b %d0, 0x8001
 	bra.s printstr
 0:	rts
 
@@ -21,8 +21,8 @@ printstr:
 intr_uart:
 	| just echo back
 	move.l %d0, -(%sp)
-	move.w 0x8000, %d0
-	move.w %d0, 0x8000
+	move.b 0x8001, %d0
+	move.b %d0, 0x8001
 	move.l (%sp)+, %d0
 	rte	
 
